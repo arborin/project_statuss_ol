c21f6e9cc84ba4b0b9e368a7e21acdfe6ce055e1
nkobaidze <nika.kobaidze@gmail.com>
2022-11-14 11:31:17 +0400

    [ADD] navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 // import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Groups from "./pages/Groups";
+import Home from "./pages/Home";
+import Nav from "./pages/Nav";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <>
+            <BrowserRouter>
+                <Nav />
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="groups" element={<Groups />} />
+                    {/* <Route path="contact" element={<Contact />} /> */}
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
