e5791c02391c38878178fc921d0c9506b3661ee7
arborin <nika.kobaidze@gmail.com>
2022-11-16 22:49:18 +0400

    [FIX] home

--- src/components/ProgressBar.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ProgressBar(props) {
-    return <div></div>;
-}
-
-export default ProgressBar;
--- src/pages/CreateGroup.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function CreateGroup() {
-    return <div></div>;
-}
-
-export default CreateGroup;
