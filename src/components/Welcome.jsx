320584dbb753515ef2f7bafdbbc57f56340262df
arborin <nika.kobaidze@gmail.com>
2022-11-16 22:39:30 +0400

    [ADD] welcome

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Welcome() {
+    return (
+        <div>
+            <p className="card-text">
+                You can create new group and add new projects and students into
+                it.
+            </p>
+            <button className="btn btn-success">Create New Group</button>
+        </div>
+    );
+}
+
+export default Welcome;
